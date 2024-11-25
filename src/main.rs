2e7065b27f368dec24d5fb03970a2e465f04a69a
seg4lt <12027886+seg4lt@users.noreply.github.com>
2024-11-25 15:12:55 -0600

    git cat-file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use anyhow::{anyhow, Context};
 use clap::{Parser, Subcommand};
-#[allow(unused_imports)]
-use std::env;
-#[allow(unused_imports)]
-use std::fs;
+use flate2::bufread::ZlibDecoder;
+use std::{
+    fs::{self, File},
+    io::{BufRead, BufReader, Read},
+    str::FromStr,
+};
 
 #[derive(Parser, Debug)]
 struct CmdArgs {
@@ -13,25 +16,77 @@ struct CmdArgs {
 #[derive(Subcommand, Debug)]
 enum Command {
     Init,
+    CatFile {
+        #[clap(short = 'p', long)]
+        pretty_print: String,
+    },
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = CmdArgs::parse();
     match args.command {
         Command::Init => init(),
+        Command::CatFile { pretty_print } => cat_file(pretty_print)?,
     }
+    Ok(())
+}
 
-    // Uncomment this block to pass the first stage
-    // let args: Vec<String> = env::args().collect();
-    // if args[1] == "init" {
-    //     fs::create_dir(".git").unwrap();
-    //     fs::create_dir(".git/objects").unwrap();
-    //     fs::create_dir(".git/refs").unwrap();
-    //     fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-    //     println!("Initialized git directory")
-    // } else {
-    //     println!("unknown command: {}", args[1])
-    // }
+fn cat_file(pretty_print: String) -> anyhow::Result<()> {
+    let folder = &pretty_print[..2];
+    let file_name = &pretty_print[2..];
+    let file = File::open(format!(".git/objects/{}/{}", folder, file_name))
+        .with_context(|| format!("unable to open file - {:?}", pretty_print))?;
+
+    let reader = BufReader::new(file);
+    let decoder = ZlibDecoder::new(reader);
+    let mut reader = BufReader::new(decoder);
+
+    let mut buf = Vec::new();
+    reader
+        .read_until(0, &mut buf)
+        .expect("unable to read header part of object file");
+    let (file_type, size) = std::str::from_utf8(&buf[..buf.len() - 1])
+        .with_context(|| format!("Unable to read file type and size: {:?}", buf))?
+        .split_once(' ')
+        .ok_or_else(|| anyhow!("Header should be two parts - {:?}", buf))
+        .and_then(|(f_type, size)| {
+            let file_type = FileType::from_str(f_type)
+                .with_context(|| format!("Invalid file type: {}", f_type))?;
+            let size = size
+                .parse::<u64>()
+                .with_context(|| format!("Invalid size: {}", size))?;
+            Ok((file_type, size))
+        })?;
+    match file_type {
+        FileType::Blob => {
+            let mut limit_reader = reader.take(size);
+            let mut stdout = std::io::stdout();
+            let _ = stdout.lock();
+            let n = std::io::copy(&mut limit_reader, &mut stdout)
+                .context("unable to copy content to stdin")?;
+            anyhow::ensure!(
+                n == size,
+                "size written({}) doesn't match size defined({})",
+                size,
+                n
+            );
+        }
+    }
+    Ok(())
+}
+
+#[derive(Debug)]
+pub enum FileType {
+    Blob,
+}
+impl FromStr for FileType {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "blob" => Ok(FileType::Blob),
+            _ => Err(anyhow!("Unrecognized filetype: {s}")),
+        }
+    }
 }
 
 fn init() {
