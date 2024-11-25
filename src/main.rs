78a62f3eaaf35ab8311eeb908dc8830fdb36c147
seg4lt <12027886+seg4lt@users.noreply.github.com>
2024-11-24 22:33:14 -0600

    Create git directory

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
+use clap::{Parser, Subcommand};
 #[allow(unused_imports)]
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
 
+#[derive(Parser, Debug)]
+struct CmdArgs {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Init,
+}
+
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    eprintln!("Logs from your program will appear here!");
+    let args = CmdArgs::parse();
+    match args.command {
+        Command::Init => init(),
+    }
 
     // Uncomment this block to pass the first stage
     // let args: Vec<String> = env::args().collect();
@@ -18,4 +32,12 @@ fn main() {
     // } else {
     //     println!("unknown command: {}", args[1])
     // }
+}
+
+fn init() {
+    fs::create_dir(".git").expect("unable to create .git directory");
+    fs::create_dir(".git/objects").expect("unable to create .git/objects");
+    fs::create_dir(".git/refs").expect("unable to create .git/refs");
+    fs::write(".git/HEAD", "ref: refs/heads/main\n").expect("unable to write to HEAD files");
+    println!("Initialized git directory")
 }
